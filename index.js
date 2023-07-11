cd84bb4f55a0111012f3e0eb4573e6ed5ef3886d
BenedictKipkuruiLangat <benedictlangat2000@gmail.com>
2023-07-11 11:46:32 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 
-// Write your code here
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+console.log(multiply); 
+// Output: 62
+
+
+let random = Math.floor(Math.random() * 100) + 1;
+console.log(random);
+
+
+let num3 = 14;
+let num4 = 5;
+let mod = num3 % num4;
+console.log(mod); 
+
+
+let numberSet = [5, 20, 10, 8, 15];
+let max=(20)
+let maxNumber = Math.max(...numberSet);
+console.log(maxNumber);
+ // Output: 20
